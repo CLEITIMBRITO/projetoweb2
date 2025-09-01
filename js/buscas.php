6dd1e90f066e818dbfc4dc7c9115e724bb99d998
ALUNO <ALUNO@LAB02-14>
2025-09-01 20:08:55 -0300

    teste2

---
+++ js/buscas.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Buscas</title>
+</head>
+<body>
+  <form name="pesquisa" action="executarbuscas.php"
+  method="post">
+  <label>Digite a pesquisa</label><br>
+  <input type="text" name="Pesquisa"><br>
+  <input type="submit" value="Pesquisa">
+
+</form>  
+</body>
+</html>
